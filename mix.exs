9de2ceeaa89b3550b3ef267ec22170481a39aca0
Cory Finger <cory.j.finger@gmail.com>
2024-06-23 23:15:07 -0700

    Remove msgpax dependency, publish to hex.pm

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Tethys.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -19,12 +20,20 @@ defmodule Tethys.MixProject do
     ]
   end
 
+  defp package do
+    [
+      description: "A networking library that guides and crafts the streams of information flowing through the modern world. Aiding the technological waters drawn from lakes, rivers, and rains as they move towards the world's oceans.",
+      links: %{"GitHub" => "https://github.com/polymorfiq-tools/tethys"},
+      licenses: ["AGPL-3.0-or-later"]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:msgpax, "~> 2.0"}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
